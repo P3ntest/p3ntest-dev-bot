72ff6560a634a00b563753e59fc94b68e1ce0a97
P3ntest <33764638+P3ntest@users.noreply.github.com>
2021-10-25 20:28:29 +0200

    dockerize

--- src/app.js
+++ src/app.js
@@ -1,52 +1,82 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { Client, Intents, MessageButton, Permissions, CommandInteraction, MessageActionRow } = require('discord.js');
-const { paymentMessageBuilder, paypalmeLinkBuilder, tebexLinkBuilder } = require('./messageBuilders');
+const {
+  Client,
+  Intents,
+  MessageButton,
+  Permissions,
+  CommandInteraction,
+  MessageActionRow,
+} = require("discord.js");
+const {
+  paymentMessageBuilder,
+  paypalmeLinkBuilder,
+  tebexLinkBuilder,
+} = require("./messageBuilders");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const messages = require("./messages");
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (interaction.isButton()) {
-    if (interaction.customId == "create_commission") {
+    if (
+      interaction.customId == "create_commission" ||
+      interaction.customId == "create_ticket"
+    ) {
+      const channelName =
+        (interaction.customId == "create_commission"
+          ? "commission-"
+          : "ticket-") + interaction.user.username;
 
-      const commissionChannel = await interaction.guild.channels.create("commission-" + interaction.user.username, {
-        topic: interaction.user.username + "'s Commission Request",
-        parent: "876945254568316948",
-        permissionOverwrites: [
-          {
-            id: interaction.user.id,
-            allow: [Permissions.FLAGS.VIEW_CHANNEL],
-          },
-          {
-            id: interaction.guild.roles.everyone,
-            deny: [Permissions.FLAGS.VIEW_CHANNEL],
-          }
-        ]
-      });
+      const commissionChannel = await interaction.guild.channels.create(
+        channelName,
+        {
+          topic: interaction.user.username + "'s Ticket",
+          parent:
+            interaction.customId == "create_commission"
+              ? "876945254568316948"
+              : "902256309376794684",
+          permissionOverwrites: [
+            {
+              id: interaction.user.id,
+              allow: [Permissions.FLAGS.VIEW_CHANNEL],
+            },
+            {
+              id: interaction.guild.roles.everyone,
+              deny: [Permissions.FLAGS.VIEW_CHANNEL],
+            },
+          ],
+        }
+      );
 
       commissionChannel.send(messageBuilder("new commission"));
 
-      await interaction.reply({ ephemeral: true, content: "Created your commission: <#" + commissionChannel.id + ">" });
+      await interaction.reply({
+        ephemeral: true,
+        content: "Created your ticket: <#" + commissionChannel.id + ">",
+      });
     } else if (interaction.customId == "delete_commission") {
       interaction.update({
-        ...messageBuilder("new commission"), components: [new MessageActionRow().addComponents(
-          new MessageButton()
-            .setLabel("Are you sure?")
-            .setStyle("DANGER")
-            .setCustomId("delete_commission_confirm")
-            .setEmoji("⚠️")
-        )]
+        ...messageBuilder("new commission"),
+        components: [
+          new MessageActionRow().addComponents(
+            new MessageButton()
+              .setLabel("Are you sure?")
+              .setStyle("DANGER")
+              .setCustomId("delete_commission_confirm")
+              .setEmoji("⚠️")
+          ),
+        ],
       });
 
       setTimeout(() => {
         try {
           interaction.message.edit(messageBuilder("new commission"));
-        } catch(e) {}
+        } catch (e) {}
       }, 5000);
     } else if (interaction.customId == "delete_commission_confirm") {
       interaction.channel.delete();
@@ -54,22 +84,21 @@ client.on('interactionCreate', async interaction => {
   }
 
   if (interaction.isCommand()) {
-    if (interaction.commandName === 'sendmsg') {
-
+    if (interaction.commandName === "sendmsg") {
       const apiMsg = messageBuilder(interaction.options.getString("message"));
 
-      if (!apiMsg)
-        await interaction.reply("Message not found.");
+      if (!apiMsg) await interaction.reply("Message not found.");
       else {
         interaction.options.getChannel("channel").send(apiMsg);
-        await interaction.reply({ content: 'Sent', ephemeral: true });
+        await interaction.reply({ content: "Sent", ephemeral: true });
       }
-
-    } else if (interaction.commandName === 'pay') {
-      await interaction.reply(paymentMessageBuilder(interaction.options.getInteger("amount")));
-    } else if (interaction.commandName === 'accept') {
+    } else if (interaction.commandName === "pay") {
+      await interaction.reply(
+        paymentMessageBuilder(interaction.options.getInteger("amount"))
+      );
+    } else if (interaction.commandName === "accept") {
       interaction.options.getMember("client").roles.add("876850716436738098");
-      await interaction.reply({ content: "✅ **Commission Accepted!**" });
+      await interaction.reply(messageBuilder("accepted"));
     }
   }
 
@@ -77,7 +106,7 @@ client.on('interactionCreate', async interaction => {
     if (interaction.customId == "payment_select_option") {
       const value = interaction.values[0];
       const amount = value.split(":")[1];
-      switch(value.split(":")[0]) {
+      switch (value.split(":")[0]) {
         case "paypal_ff":
           interaction.reply(paypalmeLinkBuilder("ff", amount));
           break;
@@ -87,11 +116,9 @@ client.on('interactionCreate', async interaction => {
         case "tebex":
           interaction.reply(tebexLinkBuilder(amount));
           break;
-          
       }
     }
   }
-
 });
 
 function messageBuilder(msgId) {
--- src/messages.js
+++ src/messages.js
@@ -1,42 +1,69 @@
-const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu } = require("discord.js");
+const {
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton,
+  MessageSelectMenu,
+} = require("discord.js");
 
 module.exports = {
-    "create commission": {
-        message: "",
-        embed: new MessageEmbed()
-            .setColor("#316ff5")
-            .setTitle("Create a commission!")
-            // .setThumbnail("https://i.imgur.com/W6XEZ0Z.png")
-            .setDescription("Click the button and send me the details about your request. I will get back to you ASAP with a quote :)")
-            .setFooter("P3ntest Development"),
-        components: [new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('create_commission')
-                    .setLabel('Create Commission')
-                    .setStyle('PRIMARY')
-                    .setEmoji("📩"),
-                new MessageButton()
-                    .setLabel("View Portfolio")
-                    .setStyle("LINK")
-                    .setURL("https://p3ntest.dev")
-            )]
-    },
-    "new commission": {
-        message: "",
-        embed: new MessageEmbed()
-            .setColor("#316ff5")
-            .setTitle("Successfully created your commission!")
-            // .setThumbnail("https://i.imgur.com/W6XEZ0Z.png")
-            .setDescription("Please tell me what you need and I will get back to you very soon!")
-            .setFooter("P3ntest Development"),
-        components: [new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('delete_commission')
-                    .setLabel('Delete Commission')
-                    .setStyle("SECONDARY")
-                    .setEmoji("⛔")
-            )]
-    }
-}
+  "create commission": {
+    message: "",
+    embed: new MessageEmbed()
+      .setColor("#316ff5")
+      .setTitle("P3ntest Development")
+      // .setThumbnail("https://i.imgur.com/W6XEZ0Z.png")
+      .setDescription(
+        "Welcome to my discord server. Use the buttons below to create commissions or tickets and I will get back to you ASAP."
+      )
+      .setFooter("P3ntest Development"),
+    components: [
+      new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId("create_commission")
+          .setLabel("Create Commission")
+          .setStyle("PRIMARY")
+          .setEmoji("📩"),
+        new MessageButton()
+          .setCustomId("create_ticket")
+          .setLabel("Create Ticket")
+          .setStyle("SUCCESS")
+          .setEmoji("💌"),
+        new MessageButton()
+          .setLabel("View Portfolio")
+          .setStyle("LINK")
+          .setURL("https://p3ntest.dev")
+      ),
+    ],
+  },
+  "new commission": {
+    message: "",
+    embed: new MessageEmbed()
+      .setColor("#316ff5")
+      .setTitle("Successfully created your ticket!")
+      // .setThumbnail("https://i.imgur.com/W6XEZ0Z.png")
+      .setDescription(
+        "Please tell me what you need and I will get back to you very soon!"
+      )
+      .setFooter("P3ntest Development"),
+    components: [
+      new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId("delete_commission")
+          .setLabel("Delete Ticket")
+          .setStyle("SECONDARY")
+          .setEmoji("⛔")
+      ),
+    ],
+  },
+  accepted: {
+    message: "",
+    embed: new MessageEmbed()
+      .setColor("#00e61f")
+      .setTitle("🧩 Commission accepted!")
+      // .setThumbnail("https://i.imgur.com/W6XEZ0Z.png")
+      .setDescription(
+        "I will start working on your commission as soon as I receive 50% of the money. You got assigned the client role."
+      )
+      .setFooter("P3ntest Development"),
+  },
+};
